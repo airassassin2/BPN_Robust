31a3cdb46c6313a8c54d518478ac047fef190ced
Perwez Akhtar <perwezakhtar29@gmail.com>
2024-03-13 20:03:14 +0530

    add more services model

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from home.models import Patient,Doctor,Appointment
+from home.models import *
 
 admin.site.register(Doctor)
 admin.site.register(Patient)
 admin.site.register(Appointment)
+admin.site.register(Restaurant)
+admin.site.register(Restaurant_user)
+admin.site.register(Restaurant_appointment)
+admin.site.register(Salon)
+admin.site.register(Salon_user)
+admin.site.register(Salon_appointment)
--- home/models.py
+++ home/models.py
@@ -30,3 +30,64 @@ class Appointment(models.Model):
     def __str__(self) -> str:
         return self.doctor.name+ " ---> " +self.patient.name
        
+
+
+
+class Restaurant(models.Model):
+    restaurant_name=models.CharField(max_length=50)
+    mobile=models.IntegerField(max_length=10)
+    address=models.CharField(max_length=100)
+    speciality=models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.restaurant_name
+    
+class Restaurant_user(models.Model):
+    name=models.CharField(max_length=50)
+    address=models.CharField(max_length=100)
+    mobile=models.CharField(max_length=10)
+    email=models.EmailField()
+
+
+    def __str__(self) -> str:
+        return self.name
+    
+    
+class Restaurant_appointment(models.Model):
+    restaurant_name=models.ForeignKey(Restaurant,on_delete=models.CASCADE)
+    name=models.ForeignKey(Restaurant_user,on_delete=models.CASCADE)
+    appointment_date=models.DateField()
+    appointment_time=models.TimeField()
+
+
+    def __str__(self) -> str:
+        return self.restaurant_name.restaurant_name +" --> "+ self.name.name
+        
+
+class Salon(models.Model):
+    salon_name=models.CharField(max_length=100)
+    mobile=models.IntegerField(max_length=10)
+    address=models.CharField(max_length=100)
+    speciality=models.CharField(max_length=100)
+
+
+class Salon_user(models.Model):
+    name=models.CharField(max_length=50)
+    address=models.CharField(max_length=100)
+    mobile=models.CharField(max_length=10)
+    email=models.EmailField()
+
+
+    def __str__(self) -> str:
+        return self.name
+
+    
+class Salon_appointment(models.Model):
+    salon_name=models.ForeignKey(Salon,on_delete=models.CASCADE)
+    name=models.ForeignKey(Salon_user,on_delete=models.CASCADE)
+    appointment_date=models.DateField()
+    appointment_time=models.TimeField()
+
+
+    def __str__(self) -> str:
+        return self.salon_name.salon_name +" --> "+ self.name.name
