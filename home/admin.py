8a11e794c2624fb68304d2a7d2a285bf86c3cac8
Perwez Akhtar <perwezakhtar29@gmail.com>
2024-03-13 14:36:07 +0530

    add mdedical class in services section

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from home.models import Patient,Doctor,Appointment
+
+admin.site.register(Doctor)
+admin.site.register(Patient)
+admin.site.register(Appointment)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 
+class Doctor(models.Model):
+    name=models.CharField(max_length=50)
+    mobile=models.IntegerField(max_length=10)
+    speciality=models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+
+class Patient(models.Model):
+    name=models.CharField(max_length=50)
+    mobile=models.IntegerField(max_length=10)
+    address=models.CharField(max_length=50)
+    email=models.EmailField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Appointment(models.Model):
+    doctor=models.ForeignKey(Doctor,on_delete=models.CASCADE)
+    patient=models.ForeignKey(Patient,on_delete=models.CASCADE)
+    appointment_date=models.DateField()
+    appointment_time=models.TimeField()
+
+    def __str__(self) -> str:
+        return self.doctor.name+ " ---> " +self.patient.name
+       
